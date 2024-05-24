16965844bd9c56406d7890cad37b4ef91a506a6c
Rodrigo M.V.S <88489374+RodrigoMvs123@users.noreply.github.com>
2024-05-24 09:02:54 -0300

    Create PromptSuggestion.tsx

---
+++ PromptSuggestion.tsx
@@ -0,0 +1,9 @@
+const PromptSuggestion = ({ text, onClick }) => {
+    return (
+        <button onClick={onClick} className="prompt-suggestion-button">
+            {text}
+        </button>
+    )
+}
+
+export default PromptSuggestion
