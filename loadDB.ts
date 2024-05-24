754d091195da8e9a64d581193bb0e47744de9f35
Rodrigo M.V.S <88489374+RodrigoMvs123@users.noreply.github.com>
2024-05-24 09:00:30 -0300

    Create loadDB.ts

---
+++ loadDB.ts
@@ -0,0 +1,54 @@
+import { DataAPIClient } from "@datastax/astra-db-ts"
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
+import "dotenv/config"
+import OpenAI from "openai"
+import sampleData from "./sample_data.json"
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+const client = new DataAPIClient(process.env.ASTRA_DB_APPLICATION_TOKEN)
+const db = client.db(process.env.ASTRA_DB_API_ENDPOINT, { namespace: process.env.ASTRA_DB_NAMESPACE })
+
+const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverLap: 200,
+})
+
+const createCollection = async () => {
+    try {
+        const res = await db.createCollection('characters', {
+            vector: {
+                dimension: 1536
+            }
+        })
+        console.log(res)
+    } catch (e) {
+        console.log('characters collections already exists')
+    }
+}
+
+const loadSampleData = async () => {
+    const collection = await db.collection('characters')
+        for await ( const { id, name, description } of SampleData) {
+            const chunks = await splitter.splitText(description)
+            let i = 0 
+            for await (const chunk of chunks) {
+                const { data } = await openai.embeddings.create({ 
+                    input: chunk, 
+                    model: 'text-embedding-ada-002'
+                })  
+                const res = await collection.insertOne({
+                    docuemnt_id: id,
+                    $vector: data[0]?.embedding,
+                    name,
+                    description: chunk
+                })
+                i++     
+            }
+        }
+        console.log('data loaded')
+}
+
+createCollection().then(() => loadSampleData())
